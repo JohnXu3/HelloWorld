95effa856f629e85dee78df33e5b1d91098a9317
John Xu <john.xu@xplusz.com>
2014-04-21 11:39:43 +0800

    second change

--- src/main/java/com/Xplusz/App.java
+++ src/main/java/com/Xplusz/App.java
@@ -8,6 +8,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        int array[] = new int[10];
+        for(int i=0;i<10;i++)
+        {
+            array[i] = (int)(Math.random()*10+10);
+
+        }
+        int temp =0;
+        for(int i=0;i<array.length;i++){
+            for(int j=array.length-1;j>i;j--){
+                if(array[j-1]>array[j]){
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        for(int i=0;i<array.length;i++){
+            System.out.println( array[i] );
+        }
     }
 }
